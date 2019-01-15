30c2acc6cf2a0897e5f89a5b6e0b7151d93de07e
8440 <drew_li@intretech.com>
2019-01-15 14:15:53 +0800

    1.增加:DI类库提交;(DNLiCore-0001)

---
+++ DNLiCore_DI/DNLiCore_DI/ServiceContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DNLiCore_DI
+{
+    public class ServiceContext
+    {
+        private static IServiceProvider _serviceProvider = null;
+        private static IServiceCollection _iserviceCollection = null;
+        public static void RegisterServices(IServiceCollection serviceCollection)
+        {
+            _iserviceCollection = serviceCollection;
+            _serviceProvider = serviceCollection.BuildServiceProvider();  //跟容器
+        }
+
+
+        /// <summary>
+        /// 获取指定注册服务
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T GetService<T>()
+        {
+            if (_serviceProvider != null)
+            {
+                return _serviceProvider.GetService<T>();
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+       
+
+    }
+}
